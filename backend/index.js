0b5ac58930435014b4445dcb30a1035b5cffa3c6
haris rehman <harisrehmanchugtai@gmail.com>
2023-08-20 23:36:47 +0500

    Update

--- backend/index.js
+++ backend/index.js
@@ -32,3 +32,16 @@ http.createServer((req, res) =>{
     res.write(JSON.stringify(data));
     res.end();
 }).listen(5000);
+
+console.log(process.argv[2]);
+const input = process.argv;
+
+if (input[2] == 'add'){
+    fs.writeFileSync(input[3], input[4]);
+}
+else if(input[2] == "remove"){
+    fs.unlinkSync(input[3])
+}
+else{
+    console.log("Invalid");
+}
